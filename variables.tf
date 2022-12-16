e7b3330d8b5a36de135eb4efd7013b91f311e5fe
Levente Simon <ls@leventesimon.com>
2022-12-16 19:53:04 +0100

    dafault vault address

--- variables.tf
+++ variables.tf
@@ -1,7 +1,10 @@
-variable "vault_address"     { type = string   }
 variable "vault_kv_path"     { type = string   }
 variable "vault_kv_policies" { type = map(any) }
 variable "vault_kv_json"     { type = string   }
+variable "vault_address" {
+  type = string
+  default = "https://127.0.0.1:8200"
+}
 variable "vault_kv_secret" {
   type      = string
   sensitive = true
